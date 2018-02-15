8eb30ac47e7e4801cc44d0205d074d44141e5948
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 08:49:26 +0900

    Create likes table

--- db/migrate/20180215234535_create_likes.rb
+++ db/migrate/20180215234535_create_likes.rb
@@ -1,10 +1,12 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.references :user, foreign_key: true
-      t.references :likeable, foreign_key: true
+      t.references :user,     foreign_key: true, null: false, index: false
+      t.references :likeable, polymorphic: true, null: false, index: true
 
       t.timestamps
     end
+
+    add_index :likes, [:user_id, :likeable_type, :likeable_id], unique: true, name: "index_likes_for_uniqueness"
   end
 end
