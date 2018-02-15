726be4fbeb9cf38d4cbf36051084c1f1dc4b0db3
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 00:21:45 +0900

    Configure RSpec

---
+++ spec/support/omni_auth_helper.rb
@@ -0,0 +1,18 @@
+module OmniAuthHelper
+  def prepare_mock_auth_from(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      "provider" => "github",
+      "uid"      => user.github_id,
+      "info" => {
+        "image"    => user.image_url,
+        "nickname" => user.nickname,
+      },
+    )
+  end
+
+  def authenticate_as(user)
+    prepare_mock_auth_from(user)
+    visit root_path
+    click_link I18n.t("shared.login")
+  end
+end
