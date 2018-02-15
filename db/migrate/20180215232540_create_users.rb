2ea2b376be56182c8106b3d3dbcb3d55f5aa6862
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 08:29:32 +0900

    Create users table

--- db/migrate/20180215232540_create_users.rb
+++ db/migrate/20180215232540_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :uid
-      t.string :nickname
-      t.string :image_url
-      t.integer :role
-      t.integer :comments_count
-      t.integer :likes_count
-      t.string :remember_token
+      t.integer :uid,            null: false, index: { unique: true }
+      t.string  :nickname,       null: false
+      t.string  :image_url,      null: true
+      t.integer :role,           null: false, default: 0, limit: 1
+      t.integer :comments_count, null: false, default: 0
+      t.integer :likes_count,    null: false, default: 0
+      t.string  :remember_token, null: false, index: { unique: true }
 
       t.timestamps
     end
