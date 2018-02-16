a5afd5725ed8e5011c6062519af5c0ff83800e42
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 23:15:50 +0900

    Impl display helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,30 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(CustomRenderer, autolink: true, tables: true, filter_html: true, fenced_code_blocks: true)
     markdown.render(text).html_safe
   end
+
+  def display_name(resource)
+    if resource.secret?
+      t("shared.anonymous")
+    else
+      resource.user.nickname
+    end
+  end
+
+  def display_link(resource)
+    if resource.secret?
+      "javascript:void(0);"
+    else
+      polymorphic_path(resource.user)
+    end
+  end
+
+  def display_image(resource)
+    if resource.secret?
+      asset_path("anonymous.svg")
+    elsif resource.user.image_url
+      polymorphic_path(resource.user.avatar)
+    else
+      asset_path("guest.png")
+    end
+  end
 end
