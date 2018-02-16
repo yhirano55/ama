2760a70b2ca79d66ef88be3217ee8c9d159211b4
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 23:16:12 +0900

    Fix assets/javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
 //= require_tree .
