f4effe940580c5d75f7a433e08d14ba8bdf631a3
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 09:34:26 +0900

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    "/auth/github/callback" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
+  resources :users, only: %i[index show]
+  resources :issues do
+    resource :like, only: :destroy, likeable_type: "Issue"
+  end
+  resources :comments do
+    resource :like, only: :destroy, likeable_type: "Comment"
+  end
+  resources :likes, only: :create
+
+  direct(:login) { "/auth/github?origin=#{request.original_url}" }
+  direct(:github) { |username_or_repo| "https://github.com/#{username_or_repo}" }
+
+  root "topics#index"
 end
