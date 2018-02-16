b18dc3df2d0891163dbc30cf8b4b06bd85907708
Yoshiyuki Hirano <yhirano@me.com>
2018-02-16 09:23:23 +0900

    Execute db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_15_230917) do
+ActiveRecord::Schema.define(version: 2018_02_15_234535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,56 @@ ActiveRecord::Schema.define(version: 2018_02_15_230917) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "issue_id", null: false
+    t.text "content", null: false
+    t.integer "likes_count", default: 0, null: false
+    t.boolean "secret", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issue_id"], name: "index_comments_on_issue_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "subject", null: false
+    t.text "description", null: false
+    t.integer "comments_count", default: 0, null: false
+    t.integer "likes_count", default: 0, null: false
+    t.boolean "secret", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_issues_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "likeable_type", null: false
+    t.bigint "likeable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
+    t.index ["user_id", "likeable_type", "likeable_id"], name: "index_likes_for_uniqueness", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "uid", null: false
+    t.string "nickname", null: false
+    t.string "image_url"
+    t.integer "role", limit: 2, default: 0, null: false
+    t.integer "comments_count", default: 0, null: false
+    t.integer "likes_count", default: 0, null: false
+    t.string "remember_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
+  end
+
+  add_foreign_key "comments", "issues"
+  add_foreign_key "comments", "users"
+  add_foreign_key "issues", "users"
+  add_foreign_key "likes", "users"
 end
