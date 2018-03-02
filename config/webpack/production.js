36dbd442bc29680a99339b2ce74ab3ff458922e1
Yoshiyuki Hirano <yhirano@me.com>
2018-03-02 14:05:30 +0900

    Refactor precompile configuration on production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,14 @@
 const environment = require('./environment')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
 
+const uglifyJsPlugin = environment.plugins.get('UglifyJs')
+uglifyJsPlugin.options = merge(uglifyJsPlugin.options, { sourceMap: false })
+
+const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+CSSLoader.options = merge(CSSLoader.options, { sourceMap: false })
+
+// https://webpack.js.org/configuration/devtool/
+// default is `nosources-source-map`
+environment.devtool = null
 module.exports = environment.toWebpackConfig()
