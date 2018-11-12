62086991f19a28b298a5b511917d78c73d7aeed4
Adrien Poly <adrienpoly@gmail.com>
2018-11-12 11:32:54 +0100

    move controllers out of packs directory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ Turbolinks.setProgressBarDelay()
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
+const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
 import "stylesheets/application"
